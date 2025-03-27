bcdee65226c8d3d724138e1b6ce06872050bdb9e
Courtcircuits <tristan-mihai.radulescue@etu.umontpellier.fr>
2025-03-27 18:17:06 +0100

    init

---
+++ credentials/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+)
+
+
+// @title Optique application TO CHANGE
+// @version 1.0
+// @description This is a sample application
+// @contact.name Courtcircuits
+// @contact.url https://github.com/Courtcircuits
+// @contact.email tristan-mihai.radulescu@etu.umontpellier.fr
+func main() {
+	conf, err := config.LoadConfig()
+	if err != nil {
+		config.HandleError(err)
+	}
+	cycle := NewCycle()
+
+	if conf.Bootstrap {
+		err := cycle.Setup()
+		if err != nil {
+			slog.Error(err.Error())
+			cycle.Stop()
+			os.Exit(1)
+		}
+	}
+
+	err = cycle.Ignite()
+}
