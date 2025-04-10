9c40917838cc1c66f0c6137baa4648404da018f0
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-04-10 20:05:26 +0200

    feat:ping elastic
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- search-service/main.go
+++ search-service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/DO-2K23-26/polypass-microservices/search-service/config"
+	"github.com/elastic/go-elasticsearch/v8"
 )
 
 // @title Optique application TO CHANGE
@@ -11,6 +12,18 @@ import (
 // @contact.url https://github.com/Courtcircuits
 // @contact.email tristan-mihai.radulescu@etu.umontpellier.fr
 func main() {
+	esConfig := elasticsearch.Config{Addresses: []string{"http://localhost:9200"}, Password: "search-password"}
+	es, _ := elasticsearch.NewClient(esConfig)
+	esPingRes, err := es.Ping()
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	if esPingRes != nil {
+
+		println(esPingRes.String())
+	}
 	conf, err := config.LoadConfig()
 	if err != nil {
 		config.HandleError(err)
