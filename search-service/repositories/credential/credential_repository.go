49d869c9553c88e917372234b364b1e3425dfb6e
Théo Tchilinguirian <theo.tchlx@proton.me>
2025-04-28 23:43:34 +0200

    feat(repositories/credential): implement CreateCredential, GetCredential

--- search-service/repositories/credential/credential_repository.go
+++ search-service/repositories/credential/credential_repository.go
@@ -1,5 +1,15 @@
 package credential
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/DO-2K23-26/polypass-microservices/search-service/infrastructure"
+
+	"github.com/DO-2K23-26/polypass-microservices/search-service/common/types"
+)
+
 type RepoCredential struct {
 	ID       string `json:"id"`
 	Title    string `json:"title"`
@@ -17,4 +27,57 @@ type ICredentialRepository interface {
 	RemoveTagsFromCredential(query RemoveTagsFromCredentialQuery) error
 }
 
+const indexName = "credential"
 
+type CredentialRepository struct {
+	esClient infrastructure.ElasticAdapter
+}
+
+func NewCredentialRepository(esClient infrastructure.ElasticAdapter) *CredentialRepository {
+	return &CredentialRepository{
+		esClient: esClient,
+	}
+}
+
+// Function to write a credential to elasticsearch
+func (c *CredentialRepository) CreateCredential(query CreateCredentialQuery) (*CreateCredentialResult, error) {
+
+	_, err := c.esClient.Client.Index(indexName).Id(query.ID).Request(query).Do(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("error indexing document ID=%s: %w", query.ID, err)
+	}
+
+	return &CreateCredentialResult{
+		Credential: RepoCredential{
+			ID:       query.ID,
+			Title:    query.Title,
+			Tags:     query.Tags,
+			FolderId: query.FolderId,
+		},
+	}, nil
+}
+
+// Function to get a credential from elasticsearch
+func (c *CredentialRepository) GetCredential(query GetCredentialQuery) (*GetCredentialResult, error) {
+	if query.ID == "" {
+		return nil, fmt.Errorf("ID is required")
+	}
+
+	res, err := c.esClient.Client.Get(indexName, query.ID).Do(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("error getting document ID=%s: %w", query.ID, err)
+	}
+
+	if !res.Found { // Document not found
+		return nil, fmt.Errorf("document ID=%s not found", query.ID)
+	}
+
+	var credential RepoCredential
+	if err := json.Unmarshal(res.Source_, &credential); err != nil {
+		return nil, fmt.Errorf("error unmarshalling document ID=%s: %w", query.ID, err)
+	}
+
+	return &GetCredentialResult{
+		Credential: credential,
+	}, nil
+}
