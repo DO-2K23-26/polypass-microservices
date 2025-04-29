7711abe234affd3be209ea1e91810133f4fdb1fb
Théo Tchilinguirian <theo.tchlx@proton.me>
2025-04-29 18:12:55 +0200

    fix(common/types/user.go): remove import

--- search-service/common/types/user.go
+++ search-service/common/types/user.go
@@ -1,7 +1,5 @@
 package types
 
-import "github.com/elastic/go-elasticsearch/v9/typedapi/types"
-
 type User struct {
 	ID        string   `json:"id" gorm:"primaryKey;column:id"`
 	Folders []Folder `json:"folders" gorm:"many2many:user_folders;"`
