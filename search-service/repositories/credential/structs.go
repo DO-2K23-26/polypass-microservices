1b70314ae9875f05239ac688eaf51f6588edde96
Théo Tchilinguirian <theo.tchlx@proton.me>
2025-04-29 18:01:59 +0200

    revert(repositories/credential): search query struct fields

--- search-service/repositories/credential/structs.go
+++ search-service/repositories/credential/structs.go
@@ -37,8 +37,11 @@ type GetCredentialResult struct {
 }
 
 type SearchCredentialQuery struct {
+	ID           string    `json:"id"`
 	Title        string    `json:"title"`
+	FolderId 	 *string   `json:"folder_id"`
 	FolderName   *string   `json:"folder_name"`
+	TagIds	     *[]string `json:"tag_ids"`
 	TagName      *string   `json:"tag_name"`
 	FoldersScope *[]string `json:"folders_scope"` // The name of the folders that the user making the request can access
 	Limit        *int      `json:"limit"`         // The limit is the maximum number of credentials to return
