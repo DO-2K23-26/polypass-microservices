4d329f33c8c66cc311733d763b700ef5abe2c886
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-05-02 14:27:02 +0200

    feat:kafka consumers and compose config
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- auhtz-service/app/app.go
+++ auhtz-service/app/app.go
@@ -1,10 +1,13 @@
 package app
 
 import (
+	"context"
 	"log"
 
 	"github.com/DO-2K23-26/polypass-microservices/authz-service/config"
+	"github.com/DO-2K23-26/polypass-microservices/authz-service/controllers/events"
 	"github.com/DO-2K23-26/polypass-microservices/authz-service/infrastructure"
+	"github.com/DO-2K23-26/polypass-microservices/authz-service/internal/consumers"
 	"github.com/authzed/authzed-go/v1"
 	"github.com/authzed/grpcutil"
 	"google.golang.org/grpc"
@@ -13,6 +16,7 @@ import (
 
 type App struct {
 	authzedClient *infrastructure.SpiceDBAdapter
+	consumers     *consumers.Consumers
 }
 
 func NewApp(config config.Config) (*App, error) {
@@ -21,23 +25,36 @@ func NewApp(config config.Config) (*App, error) {
 	if err != nil {
 		return nil, err
 	}
-	
-	authzedClient:= infrastructure.NewSpiceDBAdapter(authzedRawClient)
-	
+	kafkaClient, err := infrastructure.NewKafkaAdapter(config.KafkaHost, config.KafkaClientId)
+	if err != nil {
+		return nil, err
+	}
+
+	authzedClient := infrastructure.NewSpiceDBAdapter(authzedRawClient)
+	folderEventController := events.NewFolderEventController()
+	credentialEventController := events.NewCredentialEventController()
+	tagEventController := events.NewTagEventController()
+	userEventController := events.NewUserEventController()
+
+	consumersController := consumers.NewConsumers(folderEventController, credentialEventController, tagEventController, userEventController, *kafkaClient)
 	return &App{
+		consumers:     consumersController,
 		authzedClient: authzedClient,
 	}, nil
 }
 
 // Perform instanciation to external services/ local services/ repos
 func (a *App) Start() error {
+	ctx := context.Background()
+	a.consumers.Start(ctx)
+	<-ctx.Done()
 	return nil
 }
 
 // Perform any data migration the version of the app need
 func (a *App) Init() error {
 	if err := a.authzedClient.Migrate(); err != nil {
-		log.Println("Error while migrating:",err)
+		log.Println("Error while migrating:", err)
 		return err
 	}
 	return nil
