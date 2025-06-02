5aec6528e29d2157d04d0c23e97de5a1c529f81b
Louis Labeyrie <labeyrielouis@gmail.com>
2025-06-02 14:13:50 +0200

    Refactor Avro schema handling: update encoder logic in encoder.go, modify go.mod for dependency changes, and streamline event schemas by consolidating folder and tag events; remove obsolete schemas and generated files.
    Signed-off-by: Louis Labeyrie <labeyrielouis@gmail.com>

--- libs/avro-schemas/README.md
+++ libs/avro-schemas/README.md
@@ -1,8 +1,4 @@
 ```bash
-gogen-avro ./generated ./events/folder/created.avsc
-gogen-avro ./generated ./events/folder/updated.avsc
-gogen-avro ./generated ./events/folder/deleted.avsc
-gogen-avro ./generated ./events/tag/created.avsc
-gogen-avro ./generated ./events/tag/updated.avsc
-gogen-avro ./generated ./events/tag/deleted.avsc
+gogen-avro ./generated ./events/folder/folder.avsc
+gogen-avro ./generated ./events/tag/tag.avsc
 ```
--- libs/avro-schemas/encoder.go
+++ libs/avro-schemas/encoder.go
@@ -1,11 +1,9 @@
 package avroschemas
 
 import (
-    "github.com/riferrei/srclient"
-    "github.com/DO-2K23-26/polypass-microservices/libs/avro-schemas/schemautils"
+	"github.com/DO-2K23-26/polypass-microservices/libs/avro-schemas/schemautils"
 )
 
 func NewEncoder(schemaRegistryURL, subject, schema string) (*schemautils.AvroEncoder, error) {
-    client := srclient.CreateSchemaRegistryClient(schemaRegistryURL)
-    return schemautils.NewAvroEncoder(client, subject, schema)
+    return schemautils.NewAvroEncoder(schemaRegistryURL, subject, schema)
 }
