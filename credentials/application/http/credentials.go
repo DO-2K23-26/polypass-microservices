17a8bce916734bb7d5a2d406a1e86b8f58147e2e
pauline <pauline.contat01@etu.umontpellier.fr>
2025-06-07 19:54:41 +0200

    add card http + fmt doc

--- credentials/application/http/credentials.go
+++ credentials/application/http/credentials.go
@@ -19,15 +19,15 @@ func NewCredentialsController(service core.CredentialsService) *CredentialsContr
 }
 
 // GetPasswordCredentials godoc
-// @Summary Get password credentials
-// @Description Get a list of password credentials
-// @Tags credentials
-// @Accept json
-// @Produce json
-// @Param ids query string true "Comma-separated list of credential IDs"
-// @Success 200 {object} []types.PasswordCredential
-// @Failure 404 {object} fiber.Map
-// @Router /credentials/password [get]
+//	@Summary		Get password credentials
+//	@Description	Get a list of password credentials
+//	@Tags			credentials
+//	@Accept			json
+//	@Produce		json
+//	@Param			ids	query		string	true	"Comma-separated list of credential IDs"
+//	@Success		200	{object}	[]types.PasswordCredential
+//	@Failure		404	{object}	fiber.Map
+//	@Router			/credentials/password [get]
 func (c *CredentialsController) GetPasswordCredentials() fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 		ids_query := ctx.Query("ids")
@@ -62,16 +62,16 @@ func (c *CreatePasswordCredentialOpts) Validate(ctx *fiber.Ctx) error {
 }
 
 // CreatePasswordCredential godoc
-// @Summary Create password credential
-// @Description Create a password credential
-// @Tags credentials
-// @Accept json
-// @Produce json
-// @Param payload body CreatePasswordCredentialOpts true "Create password credential options"
-// @Success 200 {object} types.PasswordCredential
-// @Failure 400 {object} fiber.Map
-// @Failure 500 {object} fiber.Map
-// @Router /credentials/password [post]
+//	@Summary		Create password credential
+//	@Description	Create a password credential
+//	@Tags			credentials
+//	@Accept			json
+//	@Produce		json
+//	@Param			payload	body		CreatePasswordCredentialOpts	true	"Create password credential options"
+//	@Success		200		{object}	types.PasswordCredential
+//	@Failure		400		{object}	fiber.Map
+//	@Failure		500		{object}	fiber.Map
+//	@Router			/credentials/password [post]
 func (c *CredentialsController) CreatePasswordCredential() fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 		payload := new(CreatePasswordCredentialOpts)
@@ -100,17 +100,17 @@ func (c *CredentialsController) CreatePasswordCredential() fiber.Handler {
 }
 
 // UpdatePasswordCredential godoc
-// @Summary Update password credential
-// @Description Update a password credential
-// @Tags credentials
-// @Accept json
-// @Produce json
-// @Param payload body CreatePasswordCredentialOpts true "Update password credential options"
-// @Param id path string true "Credential ID"
-// @Success 200 {object} types.PasswordCredential
-// @Failure 400 {object} fiber.Map
-// @Failure 500 {object} fiber.Map
-// @Router /credentials/password/:id [put]
+//	@Summary		Update password credential
+//	@Description	Update a password credential
+//	@Tags			credentials
+//	@Accept			json
+//	@Produce		json
+//	@Param			payload	body		CreatePasswordCredentialOpts	true	"Update password credential options"
+//	@Param			id		path		string							true	"Credential ID"
+//	@Success		200		{object}	types.PasswordCredential
+//	@Failure		400		{object}	fiber.Map
+//	@Failure		500		{object}	fiber.Map
+//	@Router			/credentials/password/:id [put]
 func (c *CredentialsController) UpdatePasswordCredential() fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 		payload := new(CreatePasswordCredentialOpts)
@@ -140,16 +140,16 @@ func (c *CredentialsController) UpdatePasswordCredential() fiber.Handler {
 }
 
 // DeletePasswordCredentials godoc
-// @Summary Delete password credentials
-// @Description Delete a list of password credentials
-// @Tags credentials
-// @Accept json
-// @Produce json
-// @Param ids query string true "Comma-separated list of credential IDs"
-// @Success 200 {object} fiber.Map
-// @Failure 400 {object} fiber.Map
-// @Failure 500 {object} fiber.Map
-// @Router /credentials/password [delete]
+//	@Summary		Delete password credentials
+//	@Description	Delete a list of password credentials
+//	@Tags			credentials
+//	@Accept			json
+//	@Produce		json
+//	@Param			ids	query		string	true	"Comma-separated list of credential IDs"
+//	@Success		200	{object}	fiber.Map
+//	@Failure		400	{object}	fiber.Map
+//	@Failure		500	{object}	fiber.Map
+//	@Router			/credentials/password [delete]
 func (c *CredentialsController) DeletePasswordCredentials() fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 		ids_query := ctx.Query("ids")
@@ -173,9 +173,168 @@ func (c *CredentialsController) DeletePasswordCredentials() fiber.Handler {
 	}
 }
 
+// GetCardCredentials godoc
+//	@Summary		Get card credentials
+//	@Description	Get a list of card credentials
+//	@Tags			card
+//	@Accept			json
+//	@Produce		json
+//	@Param			ids	query		string	true	"Comma-separated list of card IDs"
+//	@Success		200	{object}	[]types.CardCredential
+//	@Failure		404	{object}	fiber.Map
+//	@Router			/card/password [get]
+func (c *CredentialsController) GetCardCredentials() fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		ids_query := ctx.Query("ids")
+		if ids_query == "" { 	
+			return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "ids is required",
+			})
+		}
+
+		ids := strings.Split(ids_query, ",")
+		credentials, err := c.service.GetCardCredentials(ids)
+		if err != nil {
+			return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		return ctx.Status(fiber.StatusOK).JSON(credentials)
+	}
+}
+
+type CreateCardCredentialOpts struct {
+	BaseValidator
+	Title        string               `json:"title" db:"title"`
+	Note         string               `json:"note" db:"note"`
+	CustomFields map[string]any       `json:"custom_fields" db:"custom_fields"`
+	types.CardAttributes
+}
+
+func (c *CreateCardCredentialOpts) Validate(ctx *fiber.Ctx) error {
+	return c.BaseValidator.Validate(ctx, c)
+}
+
+// CreateCardCredential godoc
+//	@Summary		Create card credential
+//	@Description	Create a card credential
+//	@Tags			credentials
+//	@Accept			json
+//	@Produce		json
+//	@Param			payload	body		CreateCardCredentialOpts	true	"Create card credential options"
+//	@Success		200		{object}	types.CardCredential
+//	@Failure		400		{object}	fiber.Map
+//	@Failure		500		{object}	fiber.Map
+//	@Router			/credentials/card [post]
+func (c *CredentialsController) CreateCardCredential() fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		payload := new(CreateCardCredentialOpts)
+		if err := payload.Validate(ctx); err != nil {
+			return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		cred, err := c.service.CreateCardCredential(types.CardCredential{
+			Credential: types.Credential{
+				Title:        payload.Title,
+				Note:         payload.Note,
+				CustomFields: &payload.CustomFields,
+			},
+			CardAttributes: payload.CardAttributes,
+		})
+		if err != nil {
+			return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		return ctx.Status(fiber.StatusOK).JSON(cred)
+	}
+}
+
+// UpdateCardCredential godoc
+//	@Summary		Update card credential
+//	@Description	Update a card credential
+//	@Tags			credentials
+//	@Accept			json
+//	@Produce		json
+//	@Param			payload	body		CreateCardCredentialOpts	true	"Update card credential options"
+//	@Param			id		path		string						true	"Credential ID"
+//	@Success		200		{object}	types.CardCredential
+//	@Failure		400		{object}	fiber.Map
+//	@Failure		500		{object}	fiber.Map
+//	@Router			/credentials/card/:id [put]
+func (c *CredentialsController) UpdateCardCredential() fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		payload := new(CreateCardCredentialOpts)
+		if err := payload.Validate(ctx); err != nil {
+			return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		cred, err := c.service.UpdateCardCredential(types.CardCredential{
+			Credential: types.Credential{
+				ID:           ctx.Params("id"),
+				Title:        payload.Title,
+				Note:         payload.Note,
+				CustomFields: &payload.CustomFields,
+			},
+			CardAttributes: payload.CardAttributes,
+		})
+		if err != nil {
+			return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		return ctx.Status(fiber.StatusOK).JSON(cred)
+	}
+}
+
+// DeleteCardCredentials godoc
+//	@Summary		Delete card credentials
+//	@Description	Delete a list of Card credentials
+//	@Tags			credentials
+//	@Accept			json
+//	@Produce		json
+//	@Param			ids	query		string	true	"Comma-separated list of credential IDs"
+//	@Success		200	{object}	fiber.Map
+//	@Failure		400	{object}	fiber.Map
+//	@Failure		500	{object}	fiber.Map
+//	@Router			/credentials/card [delete]
+func (c *CredentialsController) DeleteCardCredentials() fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		ids_query := ctx.Query("ids")
+		if ids_query == "" {
+			return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "ids is required",
+			})
+		}
+
+		ids := strings.Split(ids_query, ",")
+		err := c.service.DeleteCardCredentials(ids)
+		if err != nil {
+			return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+			"message": "success",
+		})
+	}
+}
+
 func (c *CredentialsController) Register(app *fiber.App) {
 	app.Get("/credentials/password", c.GetPasswordCredentials())
 	app.Post("/credentials/password", c.CreatePasswordCredential())
 	app.Put("/credentials/password/:id", c.UpdatePasswordCredential())
 	app.Delete("/credentials/password", c.DeletePasswordCredentials())
+	app.Get("/credentials/card", c.GetCardCredentials())
+	app.Post("/credentials/card", c.CreateCardCredential())
+	app.Put("/credentials/card/:id", c.UpdateCardCredential())
+	app.Delete("/credentials/card", c.DeleteCardCredentials())
 }
