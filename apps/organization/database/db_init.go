7d4a8f71b86eb33a727015994c5d7871d70aeb61
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-06-10 11:28:06 +0200

    add space in db init
    Signed-off-by: Baptiste Bronsin <baptiste.bronsin@outlook.com>

--- apps/organization/database/db_init.go
+++ apps/organization/database/db_init.go
@@ -11,6 +11,7 @@ import (
 )
 
 func main() {
+
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"), os.Getenv("POSTGRES_PORT"))
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
