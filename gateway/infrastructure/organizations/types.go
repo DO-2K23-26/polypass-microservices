f0df87f1b952409b0475808598f291994b209165
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2025-06-12 17:47:12 +0200

    feat: added types

--- gateway/infrastructure/organizations/types.go
+++ gateway/infrastructure/organizations/types.go
@@ -1,11 +1,11 @@
 package organizations
 
 type CreateFolderRequest struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Icon        string `json:"icon"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Icon        string  `json:"icon"`
 	ParentID    *string `json:"parent_id,omitempty"`
-	CreatedBy   string `json:"created_by"`
+	CreatedBy   string  `json:"created_by"`
 }
 
 type CreateFolderResponse struct {
@@ -32,8 +32,6 @@ type GetFolderResponse struct {
 	Members     []string `json:"members"`
 }
 
-
-
 type GetFoldersResponse struct {
 	Folders []GetFolderResponse `json:"folders"`
 	Total   int                 `json:"total"`
@@ -45,4 +43,42 @@ type UpdateFolderRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Icon        string `json:"icon"`
+}
+
+type GetCredentialsResponse struct {
+	Credentials []Credential `json:"credentials"`
+}
+
+type Credential struct {
+	ID           string            `json:"id"`
+	CustomFields map[string]string `json:"custom_fields,omitempty"`
+	CardCredential
+	PasswordCredential
+	SSHCredential
+	ExpiresAt      string `json:"expires_at"`
+	LastReadAt     string `json:"last_read_at"`
+	UpdatedAt      string `json:"updated_at"`
+	UserIdentifier string `json:"user_identifier"`
+	Title          string `json:"title"`
+	Note           string `json:"note"`
+}
+
+type CardCredential struct {
+	CardNumCardCredentialber string `json:"card_number"`
+	CVC                      int    `json:"cvc"`
+	ExpirationDate           string `json:"expiration_date"`
+	OwnerName                string `json:"owner_name"`
+}
+
+type PasswordCredential struct {
+	CreatedAt  string `json:"created_at"`
+	DomainName string `json:"domain_name"`
+	Password   string `json:"password"`
+}
+
+type SSHCredential struct {
+	CreatedAt  string `json:"created_at"`
+	Hostname   string `json:"hostname"`
+	PrivateKey string `json:"private_key"`
+	PublicKey  string `json:"public_key"`
 }
